8b4311a0d55a76ee54d36b901b8b95da5aa32c48
alvinyen200 <alvinyen200@gamil.com>
2023-12-06 18:58:08 +0800

    add the comment note: 2. The thought of the problem of part-02

--- src/day-01/part-02/index.js
+++ src/day-01/part-02/index.js
@@ -9,6 +9,16 @@ import fs from 'fs';
     - you would get the error like the below 
     - "Error: ENOENT: no such file or directory, open './example.txt'"
     - i.e. const lines = fs.readFileSync('./example.txt', 'utf8').split('\n');
+
+    ## Note 2. The thought of the problem of part-02
+    - find the first digit
+    -- find the first !NaN() with findIndex => A, -1
+    -- find the first matched with any of digitsInLetters with indexOf => B, -1
+    -- compare A n B to find which is smaller, ''
+    - find the last digit
+    -- find the last !NaN() with findLastIndex => A, the length
+    -- find the last matched with any of digitsInLetters with lastIndexOf => B, the length
+    -- compare A n B to find which is greater, ''
 **/
 
 try {
