c76a2c504ff85bcfafb056dd1a0c2a6c8dac73ba
alvinyen200 <alvinyen200@gmail.com>
2023-12-15 23:51:20 +0800

    read the txt files

---
+++ src/day-02/part-02/index.js
@@ -0,0 +1,6 @@
+try {
+    // const lines = fs.readFileSync('./example.txt', 'utf8').split('\n');
+    const lines = fs.readFileSync('./input.txt', 'utf8').split('\n');
+} catch (err) {
+    console.log(err);
+}
